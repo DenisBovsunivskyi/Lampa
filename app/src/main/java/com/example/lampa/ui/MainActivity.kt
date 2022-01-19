17ddd3e2cf65a25e1bbcf5b95272e8cf9fdba8b2
DenisBovsunivskyi <wweqtv1@gmail.com>
2022-01-19 23:46:48 +0200

    start of doing fragment_news.xml layout(tabs+viewpager). Added toolbar

--- app/src/main/java/com/example/lampa/ui/MainActivity.kt
+++ app/src/main/java/com/example/lampa/ui/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.lampa.ui
 
 import android.os.Bundle
+import android.view.Menu
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.lampa.R
 import com.example.lampa.base.acivity.BaseActivity
 import com.example.lampa.base.viewmodels.ViewModelSet
 import com.example.lampa.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import android.view.View
+import android.widget.Toolbar
+import androidx.appcompat.content.res.AppCompatResources
+
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
@@ -14,12 +20,28 @@ class MainActivity : BaseActivity() {
     override fun init(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        initToolbar()
+    }
+    private fun initToolbar(){
+        val toolbar= findViewById<View>(R.id.toolbar) as androidx.appcompat.widget.Toolbar
+        toolbar.navigationIcon = AppCompatResources.getDrawable(this.applicationContext,R.drawable.ic_menu_left)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = getString(R.string.text_news)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
     }
 
     override fun setNavController(): Int {
         return R.id.nav_host_fragment
 
     }
+
 
     override fun buildViewModels(): ViewModelSet {
         return ViewModelSet.Builder().build()
